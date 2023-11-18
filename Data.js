92fd71410fd588dced1edbf99eba395a9e9a8748
Abdulloh <abdullohcoder27@gmail.com>
2023-11-18 14:58:33 +0500

    date.ja

--- Data.js
+++ Data.js
@@ -1,75 +1,57 @@
 var myArray = [
     {
-        'title': 'LOWA shoes',
-        'price': '30',
+        'title': 'LOWA SHOES',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '30',
         'img': 'product1.jpg'
     },
     {
         'title': 'BeyondHome',
-        'price': '49',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '45',
         'img': 'product2.jpg'
     },
     {
-        'title': 'LOWA shoes',
-        'price': '30',
+        'title': 'BeyondHome',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '50',
         'img': 'product3.jpg'
     },
     {
         'title': 'BeyondHome',
-        'price': '49',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '50',
         'img': 'product4.jpg'
     },
     {
-        'title': 'LOWA shoes',
-        'price': '30',
+        'title': 'BeyondHome',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '5',
         'img': 'product5.jpg'
     },
     {
         'title': 'BeyondHome',
-        'price': '49',
         'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
+        'price': '45',
         'img': 'product6.jpg'
-    },
-    {
-        'title': 'LOWA shoes',
-        'price': '30',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
-        'img': 'product7.jpg'
-    },
-    {
-        'title': 'BeyondHome',
-        'price': '49',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
-        'img': 'product2.jpg'
-    },
-       {
-        'title': 'LOWA shoes',
-        'price': '30',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, suscipit...',
-        'img': 'product1.jpg'
     },
 ]
 
-buildTable(myArray)
+buildCard(myArray)
 
 
-
-function buildTable(data) {
+function buildCard(data) {
     var products = document.getElementById('myProducts')
-
-    for (var i = 0; i < data.length; i++) {
-        var row = `<div class="product_card">
-                    <img src="./public/products/${data[i].img}" alt="product">
-                    <br>
-                    <h2>${data[i].title}</h2>
-                    <p>${data[i].description}</p>
-                    <h3>Price: <span>${data[i].price}$</span></h3>
-                   </div>`
-        products.innerHTML += row
+    for(var i = 0; i < data.length; i++){
+        var product = `
+        <div class="product_card">
+                <img src="./public/products/${data[i].img}" alt="product">
+                <br>
+                <h2>${data[i].title}</h2>
+                <p>${data[i].description}</p>
+                <h3>Price: <span>${data[i].price}$</span></h3>
+        </div>
+        `
+        products.innerHTML += product;
     }
 }
